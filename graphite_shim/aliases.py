9dbfb8fb698d1dd47a228bf954cc36993df0e827
Brandon Chinn <brandonchinn178@gmail.com>
2025-10-18 13:36:55 -0700

    Prefix aliases

--- graphite_shim/aliases.py
+++ graphite_shim/aliases.py
@@ -2,21 +2,26 @@ from collections.abc import Mapping, Sequence
 from pathlib import Path
 
 ALIASES_FILE = Path.home() / ".config/graphite/aliases"
-DEFAULT_ALIASES_CONTENT = """
-ls log short
-ll log long
-ss submit --stack
-"""
+DEFAULT_ALIASES = {
+    "ls": ["log", "short"],
+    "ll": ["log", "long"],
+    "ss": ["submit", "--stack"],
+    # Technically, graphite automatically allows any prefix of a command,
+    # but we'll just represent them as aliases
+    "s": ["submit"],
+}
 
 def load_aliases() -> Mapping[str, Sequence[str]]:
     try:
         aliases_content = ALIASES_FILE.read_text()
     except FileNotFoundError:
-        aliases_content = DEFAULT_ALIASES_CONTENT
+        aliases_content = ""
 
-    return {
+    aliases = {
         alias: args
         for line in aliases_content.splitlines()
         if not line.startswith("#") and line.strip() != ""
         for alias, *args in [line.split()]
     }
+
+    return {**DEFAULT_ALIASES, **aliases}
