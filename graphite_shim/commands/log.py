e564aa6f2f5e8919e6b369e363159d1473b61f95
Brandon Chinn <brandonchinn178@gmail.com>
2025-10-17 23:46:28 -0700

    Start gt log

--- graphite_shim/commands/log.py
+++ graphite_shim/commands/log.py
@@ -6,8 +6,17 @@ class CommandLog(Command):
     """Display git history / branches."""
 
     def add_args(self, parser: argparse.ArgumentParser) -> None:
-        parser.add_argument("type", choices=["short", "long"])
-        parser.add_argument("--stack")
+        parser.add_argument("command", choices=["short", "long"], nargs="?")
+        parser.add_argument("--stack", action="store_true")
 
     def run(self, args: argparse.Namespace) -> None:
-        print("TODO: log")
+        command: str = args.command
+        only_stack: bool = args.stack
+
+        match command:
+            case None:
+                print("TODO: gt log")
+            case "short":
+                print("TODO: gt log short")
+            case "long":
+                raise NotImplementedError  # not using this yet
