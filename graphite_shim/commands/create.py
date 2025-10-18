c621b26f288981bd1aa45fb740fef41d6d0ccd2d
Brandon Chinn <brandonchinn178@gmail.com>
2025-10-17 22:39:22 -0700

    Implement create

--- graphite_shim/commands/create.py
+++ graphite_shim/commands/create.py
@@ -5,5 +5,12 @@ from graphite_shim.commands.base import Command
 class CommandCreate(Command):
     """Create a branch."""
 
+    def add_args(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument("name")
+
     def run(self, args: argparse.Namespace) -> None:
-        print("TODO: create")
+        name: str = args.name
+
+        curr = self._git.query(["branch", "--show-current"])
+        self._git.run(["switch", "-c", name])
+        self._store.branches[name] = curr
