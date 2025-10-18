ffa55db84109a6790c69099ecb11abef6587fb87
Brandon Chinn <brandonchinn178@gmail.com>
2025-10-18 12:08:19 -0700

    exec in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 here="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PYTHONPATH=$here python3 -m graphite_shim "$@"
+PYTHONPATH=$here exec python3 -m graphite_shim "$@"
