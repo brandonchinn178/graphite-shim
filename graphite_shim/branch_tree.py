e3fb8445d0a213648293771e520687e6088249a3
Brandon Chinn <brandonchinn178@gmail.com>
2025-10-21 15:43:10 -0700

    Implement `gt rename`

--- graphite_shim/branch_tree.py
+++ graphite_shim/branch_tree.py
@@ -67,11 +67,19 @@ class BranchTree:
 
     def get_branch(self, branch: str) -> BranchInfo:
         """Get the parent of the given branch."""
-        return self._branch_infos[branch]
+        try:
+            return self._branch_infos[branch]
+        except KeyError:
+            raise ValueError(f"Branch does not exist: {branch}") from None
 
     def get_branches(self) -> Sequence[BranchInfo]:
         """Get the parent of the given branch."""
         return list(self._branch_infos.values())
+
+    def rename_branch(self, *, from_: str, to: str) -> None:
+        """Rename the given branch."""
+        info = self.get_branch(from_)
+        self._parent_map = {to: info} | {k: v for k, v in self._parent_map if k != from_}
 
     def set_parent(self, branch: str, *, parent: str) -> None:
         """Set the parent of the given branch."""
