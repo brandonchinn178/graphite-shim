5b84628c65793ea6115ced5494728c6877c44eed
Brandon Chinn <brandonchinn178@gmail.com>
2025-11-03 13:59:38 -0800

    Implement `gt move`

--- graphite_shim/commands/move.py
+++ graphite_shim/commands/move.py
@@ -3,6 +3,7 @@ import dataclasses
 from collections.abc import Callable
 
 from graphite_shim.commands.base import Command
+from graphite_shim.exception import UserError
 
 
 @dataclasses.dataclass(frozen=True)
@@ -14,11 +15,22 @@ class CommandMove(Command[MoveArgs]):
     """Move a branch."""
 
     def add_args(self, parser: argparse.ArgumentParser) -> Callable[[argparse.Namespace], MoveArgs]:
-        parser.add_argument("--onto")
+        parser.add_argument("--onto", required=True)
 
         return lambda args: MoveArgs(
             onto=args.onto,
         )
 
     def run(self, args: MoveArgs) -> None:
-        print("TODO: move")
+        curr = self._git.get_curr_branch()
+        curr_branch = self._store.get_branch(curr)
+        if curr_branch.is_trunk:
+            raise UserError("Cannot move the trunk branch")
+        proc = self._git.run(
+            ["rebase", curr_branch.parent, "--onto", args.onto],
+            check=False,
+        )
+        if proc.returncode > 0:
+            self._git.run(["rebase", "--abort"], check=False)
+            raise UserError("Rebase failed")
+        self._store.set_parent(curr, parent=args.onto)
