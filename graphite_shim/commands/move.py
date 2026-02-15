aca2502db4076868f5b7b878044090379cb8bf83
Brandon Chinn <brandonchinn178@gmail.com>
2026-02-14 16:19:13 -0800

    Revert "Set parent in gt move even if rebase fails"
    This reverts commit 0be0f0afeb2e65a55182304e0a7961bd595b97b0.
    
    This doesn't work because we don't save the store on errors

--- graphite_shim/commands/move.py
+++ graphite_shim/commands/move.py
@@ -32,12 +32,11 @@ class CommandMove(Command[MoveArgs]):
             print(f"@(green){args.onto} is already the parent")
             return
 
-        self._store.set_parent(curr, parent=args.onto)
-
         proc = self._git.run(
             ["-c", "rebase.autoStash=true", "rebase", curr_branch.parent, "--onto", args.onto],
             check=False,
         )
         if proc.returncode > 0:
             self._git.run(["rebase", "--abort"], capture_output=True, check=False)
-            raise UserError("Rebase failed, rebase manually")
+            raise UserError("Rebase failed")
+        self._store.set_parent(curr, parent=args.onto)
