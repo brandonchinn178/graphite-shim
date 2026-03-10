75f64203a5c2270a025e39ee909ef684bf79ec23
Brandon Chinn <brandonchinn178@gmail.com>
2026-03-10 10:49:44 -0700

    Render branch graph better

--- graphite_shim/commands/log.py
+++ graphite_shim/commands/log.py
@@ -1,9 +1,10 @@
 import argparse
 import dataclasses
-from collections.abc import Callable
-from typing import Literal
+from collections.abc import Callable, Iterable
+from typing import Literal, Self
 
 from graphite_shim.commands.base import Command
+from graphite_shim.store import Store
 from graphite_shim.utils.term import print
 
 
@@ -39,20 +40,69 @@ class CommandLog(Command[LogArgs]):
                     parent = branch.parent if not branch.is_trunk else f"{branch.name}~1"
                     self._git.run(["log", "--oneline", "--no-decorate", f"{parent}...{branch.name}"])
             case "short":
-                if args.only_stack:
-                    starts = [curr]
-                else:
-                    starts = [branch.name for branch in self._store.get_children(self._config.trunk)]
-                    if len(starts) == 0:
-                        starts = [self._config.trunk]
-
-                stacks = [list(self._store.get_stack(start)) for start in starts]
-                for stack in sorted(stacks, key=len, reverse=True):
-                    for branch in reversed(stack):
-                        out = branch.name
-                        if branch.name == curr:
-                            out = f"@(cyan){out}"
-                        print(out)
-                    print("@(gray)" + "─" * 40)
+                graph = Graph.build(
+                    self._config.trunk,
+                    branch_filter=[curr] if args.only_stack else None,
+                    curr_branch=curr,
+                    store=self._store,
+                )
+                for line in graph.lines():
+                    print(line)
             case "long":
                 raise NotImplementedError  # not using this yet
+
+
+@dataclasses.dataclass(frozen=True)
+class Graph:
+    branches: list[tuple[int, str]]
+    curr_branch: str
+
+    @classmethod
+    def build(
+        cls,
+        trunk: str,
+        *,
+        branch_filter: list[str] | None,
+        curr_branch: str,
+        store: Store,
+    ) -> Self:
+        def _build(
+            branch: str,
+            *,
+            path_filter: list[str] | None,
+            column: int,
+        ) -> Iterable[tuple[int, str]]:
+            children = store.get_children(branch)
+            for i, child in enumerate(children):
+                if path_filter is not None:
+                    if path_filter[:1] == [child.name]:
+                        path_filter = path_filter[1:]
+                    else:
+                        continue
+                yield from _build(
+                    child.name,
+                    path_filter=path_filter,
+                    column=column + i,
+                )
+            yield (column, branch)
+
+        path_filter = None
+        if branch_filter is not None:
+            path_filter = [branch.name for branch in store.get_stack(curr_branch)]
+
+        branches = _build(trunk, path_filter=path_filter, column=0)
+        return cls(
+            branches=list(branches),
+            curr_branch=curr_branch,
+        )
+
+    def lines(self) -> Iterable[str]:
+        for col, branch in self.branches:
+            out = f"○ {branch}"
+            if branch == self.curr_branch:
+                out = f"@(cyan){out}"
+
+            prefix = ""
+            if col > 0:
+                prefix = "├" + ("─" * (col - 1))
+            yield prefix + out
