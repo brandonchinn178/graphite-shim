e9ac5de128539c23a5d26e09d736e65945d93775
Brandon Chinn <brandonchinn178@gmail.com>
2026-03-11 11:46:20 -0700

    Save parent commit on `gt continue`

--- graphite_shim/commands/continue.py
+++ graphite_shim/commands/continue.py
@@ -18,6 +18,11 @@ class CommandContinue(Command[ContinueArgs]):
         return lambda args: ContinueArgs()
 
     def run(self, args: ContinueArgs) -> None:
+        new_base = self._git.query(["rev-parse", "rebase-merge/onto"])
+
         rebase = self._git.run(["-c", "rebase.backend=apply", "rebase", "--continue"], check=False)
         if rebase.returncode > 0:
             raise UserError("Rebase failed, resolve conflicts and run `gt continue`")
+
+        curr = self._git.get_curr_branch()
+        self._store.update_parent_commit(curr, commit=new_base)
