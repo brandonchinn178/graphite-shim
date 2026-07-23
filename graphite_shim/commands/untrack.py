a12d055dfd64f34c3b5d09e494e7711a1b9dc37d
Brandon Chinn <brandonchinn178@gmail.com>
2026-07-22 17:32:46 -0700

    Allow specifying branch to untrack

--- graphite_shim/commands/untrack.py
+++ graphite_shim/commands/untrack.py
@@ -8,17 +8,20 @@ from graphite_shim.exception import UserError
 
 @dataclasses.dataclass(frozen=True)
 class UntrackArgs:
-    pass
+    branch: str | None
 
 
 class CommandUntrack(Command[UntrackArgs]):
     """Stop tracking a branch."""
 
     def add_args(self, parser: argparse.ArgumentParser) -> Callable[[argparse.Namespace], UntrackArgs]:
-        return lambda args: UntrackArgs()
+        parser.add_argument("branch")
+        return lambda args: UntrackArgs(
+            branch=args.branch,
+        )
 
     def run(self, args: UntrackArgs) -> None:
-        curr = self._git.get_curr_branch()
+        curr = args.branch or self._git.get_curr_branch()
         if curr == self._config.trunk:
             raise UserError(f"{curr} is the trunk branch")
 
